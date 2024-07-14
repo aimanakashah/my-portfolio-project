7bfe335d02015c1d7130e98777c94040feefb07f
aimanakashah <aimanakashah@gmail.com>
2024-07-14 11:02:24 +0800

    Added Header & Body components and styling in CSS.

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "./Navigation Bar";
+import Content from "./Body";
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation Bar.jsx
@@ -0,0 +1,21 @@
+import { React } from "react";
+
+function Navigation() {
+  return (
+    <header>
+      <ul className="nav">
+        <li>
+          <a href="https://example.com">About</a>
+        </li>
+        <li>
+          <a>Exhibit B</a>
+        </li>
+        <li>
+          <a>Exhibit C</a>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Navigation;
